5d005096844d7a3537e637e79470f865959576d4
Sadow0x <105595476+Sadow0x@users.noreply.github.com>
2023-12-11 20:49:18 +0100

    Simple shell 0.1

--- shell.c
+++ shell.c
@@ -15,11 +15,15 @@ int main(int ac __attribute__((unused)), char **av, char **ev)
         if (*arg == '\n')
             continue;
         if (!_strcmp(arg, "exit\n"))
-            free(arg), exit(EXIT_SUCCESS);
+        {
+            free(arg);
+            exit(EXIT_SUCCESS);
+        }
         arg[nread - 1] = '\0';
         if (*arg != '/')
         {
-            _strcpy(temp, arg), free(arg);
+            _strcpy(temp, arg);
+            free(arg);
             arg = _strcat(_strncpy(malloc(_strlen(temp) + 6), bin, 5), temp);
         }
         argv = tokenize(arg, " ");
@@ -32,8 +36,11 @@ int main(int ac __attribute__((unused)), char **av, char **ev)
         wait(&status);
         for (i = 0; argv[i]; free(argv[i++]))
             argv[i] = NULL;
-        free(argv), free(arg), _puts("$ ");
+        free(argv);
+        free(arg);
+        _puts("$ ");
     }
-    free(arg), _putchar('\n');
+    free(arg);
+    _putchar('\n');
     exit(EXIT_SUCCESS);
 }
