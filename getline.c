fb6347985b10531e4fb64e4d69d754471c26f2f3
Sadow0x <105595476+Sadow0x@users.noreply.github.com>
2023-12-14 20:41:40 +0100

    Update getline.c

--- getline.c
+++ getline.c
@@ -5,12 +5,8 @@
  * @info: parameter struct
  * @buf: address of buffer
  * @len: address of len var
- *
  * Return: bytes read
- */
-
-
-
+ **/
 ssize_t input_buf(info_t *info, char **buf, size_t *len)
 {
 	ssize_t r = 0;
